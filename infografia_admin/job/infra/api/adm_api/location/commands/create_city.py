355a7d19894138c6a75eed41fb1c2c1e05c6ca35
Giovani Liskoski Zanini <giovanilzanini@hotmail.com>
2023-02-26 23:15:29 -0300

    feat: admin location commands

---
+++ infografia_admin/job/infra/api/adm_api/location/commands/create_city.py
@@ -0,0 +1,38 @@
+from typing import Any, Coroutine, List
+
+import httpx
+from loguru import logger
+from infografia_admin.job.app.config.settings import Settings
+from infografia_admin.job.app.dto.city import CityCallbackDto, CityDto
+from infografia_admin.job.infra.base.command import BaseCommandRequest
+
+
+class CreateCity(BaseCommandRequest[CityCallbackDto]):
+    def __init__(self, data: CityDto) -> None:
+        super().__init__()
+        self.data = data
+
+    async def build_dto(
+        self, data: dict | List[dict]
+    ) -> Coroutine[Any, Any, CityCallbackDto]:
+        return CityCallbackDto(
+            id=data.get("id"),
+            state_name=data.get("state_name"),
+            name=data.get("name"),
+            microregion=data.get("microregion"),
+            mesoregion=data.get("mesoregion"),
+            immediate_region=data.get("immediate_region"),
+            middle_region=data.get("middle_region"),
+        )
+
+    async def request(
+        self, client: httpx.AsyncClient
+    ) -> Coroutine[Any, Any, httpx.Response]:
+        logger.info(f"Inserting {self.data.name} city into Admin Api")
+        response = await client.post(
+            f"{Settings.admin_api_url}/api/v1/location/city", data=self.data.dict()
+        )
+        logger.info(
+            f"Inserted {self.data.name} city into Admin Api - status code: {response.status_code} body: {response.json()}"
+        )
+        return response
---
+++ infografia_admin/job/infra/api/adm_api/location/commands/create_state.py
@@ -0,0 +1,32 @@
+from typing import List
+
+import httpx
+from loguru import logger
+from infografia_admin.job.app.config.settings import Settings
+from infografia_admin.job.app.dto.state import StateCallbackDto, StateDto
+from infografia_admin.job.infra.base.command import BaseCommandRequest
+
+
+class CreateState(BaseCommandRequest[StateCallbackDto]):
+    def __init__(self, data: StateDto) -> None:
+        super().__init__()
+        self.data = data
+
+    async def build_dto(self, data: dict | List[dict]) -> StateCallbackDto:
+        return StateCallbackDto(
+            id=data.get("id"),
+            state_acronym=data.get("state_acronym"),
+            state_name=data.get("state_name"),
+            region_acronym=data.get("region_acronym"),
+            region_name=data.get("region_name"),
+        )
+
+    async def request(self, client: httpx.AsyncClient) -> httpx.Response:
+        logger.info(f"Inserting {self.data.state_name} state into Admin Api")
+        response = await client.post(
+            f"{Settings.admin_api_url}/api/v1/location/state", data=self.data.dict()
+        )
+        logger.info(
+            f"inserted {self.data.state_name} state into Admin Api - status code: {response.status_code} body: {response.json()}"
+        )
+        return response
