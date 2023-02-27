5e196571aad5601d05e4c05174da1d375df2ea65
Giovani Liskoski Zanini <giovanilzanini@hotmail.com>
2023-02-26 23:14:21 -0300

    feat: weatherstack commands

---
+++ infografia_admin/job/infra/api/weatherstack/commands/get_current_weather.py
@@ -0,0 +1,49 @@
+from typing import List
+
+import httpx
+from infografia_admin.job.app.config.settings import Settings
+from infografia_admin.job.app.dto.weather import WeatherDto
+from infografia_admin.job.infra.base.command import BaseCommandRequest
+from loguru import logger
+
+
+class GetCurrentWeatherByCity(BaseCommandRequest[WeatherDto]):
+    def __init__(self, city_name: str) -> None:
+        self.city_name = city_name
+
+    async def build_dto(self, data: dict | List[dict]) -> WeatherDto:
+        current = data.get("current", {})
+        return WeatherDto(
+            temperature=current.get("temperature", 0),
+            wind_speed=current.get("wind_speed", 0),
+            wind_degree=current.get("wind_degree", 0),
+            pressure=current.get("pressure", 0),
+            precip=current.get("precip", 0),
+            humidity=current.get("humidity", 0),
+            cloudcover=current.get("cloudcover", 0),
+            feelslike=current.get("feelslike", 0),
+            uv_index=current.get("uv_index", 0),
+            city_name=self.city_name,
+            observation_time=current.get("observation_time", ""),
+            wind_dir=current.get("wind_dir", ""),
+        )
+
+    async def request(self, client: httpx.AsyncClient) -> httpx.Response:
+        logger.info(f"Getting weather from {self.city_name}")
+        try:
+            response = await client.get(
+                f"{Settings.weatherstack_api_url}/current",
+                params={
+                    "access_key": Settings.weatherstack_api_token,
+                    "query": self.city_name,
+                },
+            )
+        except Exception as err:
+            logger.error(
+                f"An error occurred when Getting weather from {self.city_name} - error: {err}"
+            )
+            return None
+        logger.info(
+            f"Getted weather from {self.city_name} - status code: {response.status_code} body: {response.json()}"
+        )
+        return response
